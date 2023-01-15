129c1608100a15585684ed4096a21f6d6ff167a8
Sung-En-Huang <s385huan@uwaterloo.ca>
2023-01-14 21:39:15 -0500

    Testing Subkey result
    Testing Subkey result

--- server/attraction.py
+++ server/attraction.py
@@ -4,13 +4,13 @@ location = input ("Enter city: ")
 
 params = {
   "engine": "google",
-  "q": "Top sights in " + location,
+  "q": "Top attractions in " + location,
   "location": location,
   "api_key": "344a2272107bc37f6bb8abf4dad08e5b64c50d95144a506a8415377f25e365f9"
 }
 
 search = GoogleSearch(params)
 results = search.get_dict()
-top_sights = results["top_sights"]
+sights = results.get("top_sights").get("sights")
 
-print (results)
+print (sights)
